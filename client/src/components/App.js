a07ac644b3fc4377db83b126d8b00a223d3a123a
Huynh Hoang <mich.hthuynh@gmail.com>
2020-12-09 10:54:01 +0000

    setup client

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import materializeCSS from 'materialize-css/dist/css/materialize.min.css'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
--- services/passport.js
+++ services/passport.js
@@ -23,17 +23,14 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-  }, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleID: profile.id }).then(existingUser => {
-      if (existingUser) {
-
-        // we already have a record with the given profile ID
-        done(null, existingUser)
-      } else {
-        new User({ googleID: profile.id })
-          .save()
-          .then(user => done(null, user))
-      }
-    })
+  }, async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleID: profile.id })
+    if (existingUser) {
+      // we already have a record with the given profile ID
+      return done(null, existingUser)
+    } else {
+      const user = await new User({ googleID: profile.id }).save()
+      return done(null, user)
+    }
   })
 )
